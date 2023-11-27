92088d693d12f8874a33cefef6ece33729267716
S566 <142957818+Sneha024@users.noreply.github.com>
2023-11-27 12:23:28 -0600

    added login and registration page

--- routes/book.js
+++ routes/book.js
@@ -1,6 +1,14 @@
 var express = require('express');
 const book_controllers= require('../controllers/book');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
 
 /* GET costumes */
 router.get('/', book_controllers.book_view_all_Page );
@@ -10,6 +18,6 @@ router.get('/detail', book_controllers.book_view_one_Page);
 /* GET create book page */
 router.get('/create', book_controllers.book_create_Page);
 /* GET create update page */
-router.get('/update', book_controllers.book_update_Page);
+router.get('/update',secured, book_controllers.book_update_Page);
 /* GET delete book page */
 router.get('/delete', book_controllers.book_delete_Page);
